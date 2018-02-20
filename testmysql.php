9b78ae71e59c59d447874b53cd5dae3d060ceccd
achoyjun <34291972+achoyjun@users.noreply.github.com>
2018-02-20 21:01:39 +0800

    Add files via upload

---
+++ testmysql.php
@@ -0,0 +1,55 @@
+<?php
+$mysql_hostname = "localhost";
+	$mysql_user = "root";
+	$mysql_password = "";
+	$mysql_database = "simple_login";
+	$prefix = "";
+	$bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+	mysqli_select_db($bd, $mysql_database) or die("Could not select database");
+	
+	session_start();	
+	
+	unset($_SESSION['SESS_MEMBER_ID']);
+	unset($_SESSION['SESS_FIRST_NAME']);
+	unset($_SESSION['SESS_LAST_NAME']);
+?>
+
+<html>
+<head>
+	<title>SAMPLE</title>
+</head>
+<body background img src="C:\wamp64\www\aa.jpg">
+<form name="loginform" action="login_exec.php" method="post">
+<table width="309" border="0" align="center" cellpadding="2" cellspacing="5">
+  <tr>
+    <td colspan="2">
+		
+		 <?php
+			if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
+			echo '<ul class="err">';
+			foreach($_SESSION['ERRMSG_ARR'] as $msg) {
+				echo '<li>',$msg,'</li>'; 
+				}
+			echo '</ul>';
+			unset($_SESSION['ERRMSG_ARR']);
+			}
+		?>
+	</td>
+  </tr>
+  <tr>
+  	<br>
+    <td width="116"><div align="right">Username</div></td>
+    <td width="177"><input name="username" type="text" /></td>
+  </tr>
+  <tr>
+    <td><div align="right">Password</div></td>
+    <td><input name="password" type="password" /></td>
+  </tr>
+  <tr>
+    <td><div align="right"></div></td>
+    <td><input name="" type="submit" value="login" /></td>
+  </tr>
+</table>
+</form>
+</body>
+</html>	
